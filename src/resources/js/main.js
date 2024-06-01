984cbb6e66ad8e8235e4cc5176736a1525e6d94e
DeadBranches <56521874+DeadBranches@users.noreply.github.com>
2024-06-01 17:17:00 -0400

    chore: cleanup commented code

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -15,50 +15,8 @@ import { initializeIconSearchFabEventHandler, initializeIconSearchTabEventHandle
 createIconSearchFab();
 document.addEventListener('DOMContentLoaded', async () => {  // window.addEventListener('load', () => {
     await initializeIconSearchDialog();
-
     initializeIconSearchFabEventHandler();
     initializeIconSearchTabEventHandler();
     window.Alpine = Alpine;
     Alpine.start();
 });
-
-
-// Select your Material Design component
-const materialComponent = document.getElementById('icon-search-main-dialog');
-
-// Wait for the component to be fully loaded
-materialComponent.addEventListener('load', () => {
-  // Access the shadow root of the component
-  const shadowRoot = materialComponent.shadowRoot;
-
-  // Create a style element
-  const style = document.createElement('style');
-  style.textContent = `
-  .scrollable .scroller {
-    overflow-y: hidden !important;
-  }
-    .scroller {
-      overflow-y: hidden !important;
-    }
-  `;
-
-  // Append the style to the shadow root
-  shadowRoot.appendChild(style);
-});
-
-
-// load css files
-// (function() {
-//     const cssFile = document.createElement('link');
-//     cssFile.rel = 'stylesheet';
-//     cssFile.type = 'text/css';
-//     cssFile.href = logseqAssetPath() + '/tabler-icon-search-ui/css/main.css';
-//     document.head.appendChild(cssFile);
-// })();
-// (function() {
-//     const cssFile = document.createElement('link');
-//     cssFile.rel = 'stylesheet';
-//     cssFile.type = 'text/css';
-//     cssFile.href = logseqAssetPath() + '/tabler-icon-search-ui/css/material-design/light.css';
-//     document.head.appendChild(cssFile);
-// })();
