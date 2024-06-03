dc3dfbc6575e0b33b5ace1647bf8c6aac77e3a2f
DeadBranches <56521874+DeadBranches@users.noreply.github.com>
2024-06-03 08:06:56 -0400

    refactor: styles to ./main.css

--- src/main.js
+++ src/main.js
@@ -1,19 +1,4 @@
-let process = 'dev';
-// Styles
-// import { loadGoogleFonts } from './utils/loadFont.js';
-// loadGoogleFonts();
-// Conditionally import font-face.css based on the environment
-if (process === 'dev') {
-  let styleElement = document.createElement('link');
-  styleElement.rel = 'stylesheet';
-  styleElement.href = './styles/font-face.dev.css';
-  document.head.appendChild(styleElement);
-} else {
-    // Use dynamic asset path depending on platform
-    import('./utils/font-face.prod.js');
-}
-
-// Import dependencies
+// Framework dependancies
 import './utils/material-components.js'
 import Alpine from 'alpinejs';
 
