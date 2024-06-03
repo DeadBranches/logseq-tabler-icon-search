053f316f8798c2bb78ca3c8ef144514337b54250
DeadBranches <56521874+DeadBranches@users.noreply.github.com>
2024-06-03 07:52:49 -0400

    last known working

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,7 +7,7 @@
 // import minifyHTML from 'rollup-plugin-minify-html-literals';
 // import summary from 'rollup-plugin-summary';
 // Import rollup plugins
-
+import commonjs from '@rollup/plugin-commonjs';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { copy } from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
@@ -16,50 +16,60 @@ import terser from '@rollup/plugin-terser';
 // const minifyHTML = pkgMinifyHTML.default;
 import summary from 'rollup-plugin-summary';
 
-
 export default [
+  // First pass for material-components.js
   {
-    input: 'src/environments/logseq/custom.js',
+    input: 'src/utils/material-components.js',
     output: {
-      file: 'dist/custom.js',
-      format: 'iife'
+      file: 'temp/material-components.bundle.js',
+      format: 'esm',
     },
-    plugins: []
-  },
-  {
-    input: 'src/resources/js/material-web-components.js',
     plugins: [
-      // Entry point for application build; can specify a glob to build multiple
-      // HTML files for non-SPA app
-      // html({
-      //   input: 'index.html',
-      // }),
-
-      // Resolve bare module specifiers to relative paths
       resolve(),
-
-      // Minify HTML template literals
-      // minifyHTML(),
-
-      // Minify JS
-
+      commonjs(),
       terser({
         ecma: 2021,
         module: true,
         warnings: true,
         output: { comments: false } // https://stackoverflow.com/questions/60851390/svelte-bundle-js-is-large-full-of-license-comments-even-in-production-mode
       }),
-      // Print bundle summary
-      summary(),
-      // Optional: copy any static assets to build directory
-
-      // copy({
-      //   patterns: ['images/**/*'],
-      // }),
+      summary()
     ],
-    output: {
-      dir: 'build',
-    },
-    preserveEntrySignatures: 'strict',
+    treeshake: true // Enable tree-shaking
   },
+  // Second pass for main.js
+  {
+    input: 'src/main.js',
+    output: {
+      file: 'dist/logseq-tabler-icon-search/bundle.js',
+      format: 'iife',
+      name: 'MyBundle',
+      inlineDynamicImports: true
+    },
+    plugins: [
+      // Include the intermediate file without further processing
+      {
+        name: 'include-material-components',
+        resolveId(source) {
+          if (source === './utils/material-components.js') {
+            return 'temp/material-components.bundle.js';
+          }
+          return null;
+        }
+      },
+      copy({
+        targets: [
+          { src: 'src/environments/logseq/custom.js', dest: 'dist/logseq-tabler-icon-search' },
+          { src: 'src/styles/*', dest: 'dist/logseq-tabler-icon-search/css' }
+          // { src: 'src/', dest: 'dist/logseq-tabler-icon-search/' },
+          // { src: 'src/', dest: 'dist/logseq-tabler-icon-search/' },
+
+        ],
+        verbose: true
+      }),
+      summary(),
+
+    ],
+    treeshake: false // Disable tree-shaking for this pass
+  }
 ];
---
+++ rollup.config.mjs.o.mjs
@@ -0,0 +1,65 @@
+// // Import rollup plugins
+
+// import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+// import {copy} from '@web/rollup-plugin-copy';
+// import resolve from '@rollup/plugin-node-resolve';
+// import { default as terser } from '@rollup/plugin-terser';
+// import minifyHTML from 'rollup-plugin-minify-html-literals';
+// import summary from 'rollup-plugin-summary';
+// Import rollup plugins
+
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import { copy } from '@web/rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+// import pkgMinifyHTML from 'rollup-plugin-minify-html-literals';
+// const minifyHTML = pkgMinifyHTML.default;
+import summary from 'rollup-plugin-summary';
+
+
+export default [
+  {
+    input: 'src/environments/logseq/custom.js',
+    output: {
+      file: 'dist/logseq-tabler-icon-search/custom.js',
+      format: 'iife'
+    },
+    plugins: []
+  },
+  {
+    input: 'src/resources/js/material-web-components.js',
+    plugins: [
+      // Entry point for application build; can specify a glob to build multiple
+      // HTML files for non-SPA app
+      // html({
+      //   input: 'index.html',
+      // }),
+
+      // Resolve bare module specifiers to relative paths
+      resolve(),
+
+      // Minify HTML template literals
+      // minifyHTML(),
+
+      // Minify JS
+
+      terser({
+        ecma: 2021,
+        module: true,
+        warnings: true,
+        output: { comments: false } // https://stackoverflow.com/questions/60851390/svelte-bundle-js-is-large-full-of-license-comments-even-in-production-mode
+      }),
+      // Print bundle summary
+      summary(),
+      // Optional: copy any static assets to build directory
+
+      // copy({
+      //   patterns: ['images/**/*'],
+      // }),
+    ],
+    output: {
+      dir: 'build',
+    },
+    preserveEntrySignatures: 'strict',
+  },
+];
